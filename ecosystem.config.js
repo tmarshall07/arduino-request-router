2f8d72ec9a8d8cb2627b53aa8d56a14bdf1edde6
Greg Antrim <g.antrim@kunzleigh.com>
2019-01-15 19:45:43 -0500

    add ecosystem and more advanced routes

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [{
+        name: 'arduino-request-router',
+        script: 'bin/www',
+        // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+        env: {
+            NODE_ENV: 'development',
+            PORT: 3000
+        },
+        env_production: {
+            NODE_ENV: 'production',
+            PORT: 3000
+        }
+    }]
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,68 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const rp = require('request-promise');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function (req, res, next) {
+    res.render('index');
+});
+
+// Forward request directly to arduino on local network
+router.post('/forward-directly-to-arduino', async function (req, res) {
+    // TODO: Replace this with the local IP address of your arduino
+    const arduinoIpAddress = '192.168.10.136';
+    const arduinoUrl = 'http://'+arduinoIpAddress;
+    const requestJson = req.body;
+
+    const requestOptions = {
+        method: 'POST',
+        uri: arduinoUrl,
+        body: requestJson,
+        json: true
+    };
+
+    await rp(requestOptions);
+
+    res.send(200);
+});
+
+// Send request to correct device based on a request parameter
+router.post('/complex-request/:device', async function (req, res) {
+    // TODO: Replace these with the local IP addresses of your arduinos
+    const arduino1Url = 'http://192.168.10.136';
+    const arduino2Url = 'http://192.168.10.137';
+
+    // get the device from the request parameters
+    const deviceToSendCommandTo = req.params.device;
+
+    const requestOptions = {
+        method: 'POST',
+        body: {
+            action:'off'
+        },
+        json: true
+    };
+
+    // send request to arduino 1
+    if(deviceToSendCommandTo==='1'){
+        requestOptions.uri=arduino1Url;
+        await rp(requestOptions);
+    }
+    // send request to arduino 2
+    else if(deviceToSendCommandTo==='2'){
+        requestOptions.uri=arduino2Url;
+        await rp(requestOptions);
+    }
+    // send requests to both arduinos
+    else if(deviceToSendCommandTo==='all'){
+        requestOptions.uri=arduino1Url;
+        await rp(requestOptions);
+
+        requestOptions.uri=arduino2Url;
+        await rp(requestOptions);
+    }
+
+    res.send(200);
 });
 
 module.exports = router;
